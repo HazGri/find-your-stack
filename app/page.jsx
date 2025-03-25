e24def71015b08ba19b5119716929ae45a290042
HazGri <hazemgherissi@gmail.com>
2025-03-25 11:25:10 +0100

    nextAuth add

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,11 @@
 
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
-import { HeroInput } from "@/myComponents/heroInput"; // Assure-toi que le chemin d'import est correct
+import { HeroInput } from "@/myComponents/heroInput"; 
 import { SignupForm } from "@/myComponents/signForm";
 import { motion } from "framer-motion";
+import { Header } from "@/myComponents/Header";
+import { LoginForm } from "@/myComponents/loginForm";
 
 export default function Home() {
   const [prompt, setPrompt] = useState("");
@@ -12,9 +14,16 @@ export default function Home() {
   const [stack, setStack] = useState(null);
   const [error, setError] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(false);
 
   const openModal = () => {
     setIsModalOpen(true);
+  };
+  const openLoginModal = () => {
+    setIsLoginModalOpen(true);
+  };
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
   };
   const closeModal = () => {
     setIsModalOpen(false);
@@ -48,29 +57,40 @@ export default function Home() {
 
   return (
     <div className="flex flex-col px-2">
+      <Header onClick={openLoginModal}/>
+      {isLoginModalOpen ? (
+        <motion.div
+          className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-opacity-50 z-50"
+          initial={{ opacity: 0 }} 
+          animate={{ opacity: 1 }} 
+          exit={{ opacity: 0 }} 
+          transition={{ duration: 0.3 }}
+        >
+          <LoginForm closeModal={closeLoginModal}/>
+        </motion.div>
+      ) : null}
       <HeroInput
-        onChange={handleChange} // Passe la valeur de l'input au state 'prompt'
-        onSubmit={handleSubmit} // Soumet le formulaire
+        onChange={handleChange} 
+        onSubmit={handleSubmit} 
       />
+          <button
+            onClick={openModal}
+            className="cursor-pointer self-center px-4 w-fit py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
+          >
+            Sign up to save your stack
+          </button>
 
       {loading ? (
         <p className="loading self-center loading-spinner loading-xl"></p>
       ) : null}
-      <button
-        onClick={openModal}
-        className="cursor-pointer self-center px-4 w-fit py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
-      >
-        Sign up to save your stack
-      </button>
       {isModalOpen ? (
         <motion.div
           className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-opacity-50 z-50"
-          initial={{ opacity: 0 }} // Initial state: modal invisible
-          animate={{ opacity: 1 }} // Animate to full opacity when modal is open
-          exit={{ opacity: 0 }} // Animate to invisible when modal is closed
+          initial={{ opacity: 0 }} 
+          animate={{ opacity: 1 }} 
+          exit={{ opacity: 0 }} 
           transition={{ duration: 0.3 }}
         >
-          
           <SignupForm closeModal={closeModal} />
         </motion.div>
       ) : null}
@@ -78,100 +98,102 @@ export default function Home() {
       {stack && (
         <div className="space-y-8 p-4 flex flex-col">
           {/* Front-end Section */}
-          <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
-            <p className="text-2xl font-semibold mb-4">Front-end</p>
-            <ul>
-              <li>
-                <strong>Framework:</strong> {stack.front_end.framework}
-              </li>
-              <li>
-                <strong>State Management:</strong>{" "}
-                {stack.front_end.state_management}
-              </li>
-              <li>
-                <strong>Styling:</strong> {stack.front_end.styling}
-              </li>
-              <li>
-                <strong>Mapping:</strong> {stack.front_end.mapping}
-              </li>
-              <li>
-                <strong>Additional Libraries:</strong>{" "}
-                {stack.front_end.additional_libraries.join(", ")}
-              </li>
-              <li>
-                <strong>Explanation:</strong> {stack.front_end.explanation}
-              </li>
-              <li>
-                <strong>Alternative:</strong> {stack.front_end.alternative}
-              </li>
-              <li>
-                <strong>Alternative Explanation:</strong>{" "}
-                {stack.front_end.alternative_explanation}
-              </li>
-            </ul>
-          </div>
+          <div className="flex flex-col gap-2 md:flex-row">
+            <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
+              <p className="text-2xl font-semibold mb-4">Front-end</p>
+              <ul>
+                <li>
+                  <strong>Framework:</strong> {stack.front_end.framework}
+                </li>
+                <li>
+                  <strong>State Management:</strong>{" "}
+                  {stack.front_end.state_management}
+                </li>
+                <li>
+                  <strong>Styling:</strong> {stack.front_end.styling}
+                </li>
+                <li>
+                  <strong>Mapping:</strong> {stack.front_end.mapping}
+                </li>
+                <li>
+                  <strong>Additional Libraries:</strong>{" "}
+                  {stack.front_end.additional_libraries.join(", ")}
+                </li>
+                <li>
+                  <strong>Explanation:</strong> {stack.front_end.explanation}
+                </li>
+                <li>
+                  <strong>Alternative:</strong> {stack.front_end.alternative}
+                </li>
+                <li>
+                  <strong>Alternative Explanation:</strong>{" "}
+                  {stack.front_end.alternative_explanation}
+                </li>
+              </ul>
+            </div>
 
-          {/* Back-end Section */}
-          <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
-            <h3 className="text-2xl font-semibold mb-4">Back-end</h3>
-            <ul>
-              <li>
-                <strong>Framework:</strong> {stack.back_end.framework}
-              </li>
-              <li>
-                <strong>Web Framework:</strong> {stack.back_end.web_framework}
-              </li>
-              <li>
-                <strong>Authentication:</strong> {stack.back_end.authentication}
-              </li>
-              <li>
-                <strong>Real-time:</strong> {stack.back_end.real_time}
-              </li>
-              <li>
-                <strong>Additional Services:</strong>{" "}
-                {stack.back_end.additional_services.join(", ")}
-              </li>
-              <li>
-                <strong>Explanation:</strong> {stack.back_end.explanation}
-              </li>
-              <li>
-                <strong>Alternative:</strong> {stack.back_end.alternative}
-              </li>
-              <li>
-                <strong>Alternative Explanation:</strong>{" "}
-                {stack.back_end.alternative_explanation}
-              </li>
-            </ul>
-          </div>
+            {/* Back-end Section */}
+            <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold mb-4">Back-end</h3>
+              <ul>
+                <li>
+                  <strong>Framework:</strong> {stack.back_end.framework}
+                </li>
+                <li>
+                  <strong>Web Framework:</strong> {stack.back_end.web_framework}
+                </li>
+                <li>
+                  <strong>Authentication:</strong>{" "}
+                  {stack.back_end.authentication}
+                </li>
+                <li>
+                  <strong>Real-time:</strong> {stack.back_end.real_time}
+                </li>
+                <li>
+                  <strong>Additional Services:</strong>{" "}
+                  {stack.back_end.additional_services.join(", ")}
+                </li>
+                <li>
+                  <strong>Explanation:</strong> {stack.back_end.explanation}
+                </li>
+                <li>
+                  <strong>Alternative:</strong> {stack.back_end.alternative}
+                </li>
+                <li>
+                  <strong>Alternative Explanation:</strong>{" "}
+                  {stack.back_end.alternative_explanation}
+                </li>
+              </ul>
+            </div>
 
-          {/* Database Section */}
-          <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
-            <h3 className="text-2xl font-semibold mb-4">Database</h3>
-            <ul>
-              <li>
-                <strong>Type:</strong> {stack.database.type}
-              </li>
-              <li>
-                <strong>Database:</strong> {stack.database.database}
-              </li>
-              <li>
-                <strong>Cloud Service:</strong> {stack.database.cloud_service}
-              </li>
-              <li>
-                <strong>Explanation:</strong> {stack.database.explanation}
-              </li>
-              <li>
-                <strong>Alternative:</strong> {stack.database.alternative}
-              </li>
-              <li>
-                <strong>Alternative Explanation:</strong>{" "}
-                {stack.database.alternative_explanation}
-              </li>
-            </ul>
+            {/* Database Section */}
+            <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
+              <h3 className="text-2xl font-semibold mb-4">Database</h3>
+              <ul>
+                <li>
+                  <strong>Type:</strong> {stack.database.type}
+                </li>
+                <li>
+                  <strong>Database:</strong> {stack.database.database}
+                </li>
+                <li>
+                  <strong>Cloud Service:</strong> {stack.database.cloud_service}
+                </li>
+                <li>
+                  <strong>Explanation:</strong> {stack.database.explanation}
+                </li>
+                <li>
+                  <strong>Alternative:</strong> {stack.database.alternative}
+                </li>
+                <li>
+                  <strong>Alternative Explanation:</strong>{" "}
+                  {stack.database.alternative_explanation}
+                </li>
+              </ul>
+            </div>
           </div>
-
           {/* Additional Features Section */}
-          <div className="bg-white border-2 border-gray-300 p-6 rounded-lg shadow-lg">
+          <div className="bg-white border-2 border-gray-300 p-6 mx-auto rounded-lg shadow-lg">
             <h3 className="text-2xl font-semibold mb-4">Additional Features</h3>
             <ul>
               {stack.additional_features.map((feature, index) => (
--- myComponents/signForm.jsx
+++ myComponents/signForm.jsx
@@ -3,17 +3,36 @@ import { Label } from "../components/ui/label";
 import { Input } from "../components/ui/input";
 import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
-
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-} from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandGoogle } from "@tabler/icons-react";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
-export function SignupForm({closeModal}) {
-  const handleSubmit = (e) => {
+export function SignupForm({ closeModal }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
-    console.log("Form submitted");
+    try {
+      const response = await fetch("/api/saveUser", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, userPassword }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Erreur lors de la création de l'utilisateurs");
+      }
+      const data = await response.json();
+      console.log("Réponse de l'API : ", data); 
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
   return (
     <div className=" max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
@@ -27,50 +46,67 @@ export function SignupForm({closeModal}) {
         <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname">User name</Label>
-            <Input id="firstname" placeholder="Tyler" type="text" />
+            <Input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              id="firstname"
+              placeholder="Tyler"
+              type="text"
+            />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
-          <Input id="email" placeholder="johnDoe@gmail.com" type="email" />
+          <Input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            id="email"
+            placeholder="johnDoe@gmail.com"
+            type="email"
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input
+            onChange={(e) => setUserPassword(e.target.value)}
+            value={userPassword}
+            id="password"
+            placeholder="••••••••"
+            type="password"
+          />
         </LabelInputContainer>
-
         <button
           className="cursor-pointer bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
-        >
-          Sign up &rarr;
-          <BottomGradient />
+          >
+          {loading ?  <p className="loading self-center loading-spinner loading-md"></p> : ( <>Sign up &rarr;
+          <BottomGradient /> </>) }
         </button>
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
         <div className="flex flex-col space-y-4">
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
+          <button className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]">
             <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
               GitHub
             </span>
             <BottomGradient />
           </button>
-          <button
-            className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
-            type="submit"
-          >
+          <button className=" relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]">
             <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
             <span className="text-neutral-700 dark:text-neutral-300 text-sm">
               Google
             </span>
             <BottomGradient />
           </button>
-        <Button className="cursor-pointer" onClick={closeModal} variant="secondary" >Close</Button>
+          <Button
+            className="cursor-pointer"
+            onClick={closeModal}
+            variant="secondary"
+          >
+            Close
+          </Button>
         </div>
       </form>
     </div>
